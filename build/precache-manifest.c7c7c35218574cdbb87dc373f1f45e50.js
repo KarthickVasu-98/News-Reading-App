0763e6c448274ab67fd0a255c54e2014c53ab88d
Karthick Vasu <65020616+KarthickVasu-98@users.noreply.github.com>
2020-09-28 01:06:00 +0530

    Add files via upload

---
+++ build/precache-manifest.c7c7c35218574cdbb87dc373f1f45e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23668887abb346e29ad1e4c692cdc22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d5aabd2bd1c8c26b6c",
+    "url": "/static/css/main.158072b2.chunk.css"
+  },
+  {
+    "revision": "f89659e3092840c259f7",
+    "url": "/static/js/2.977ca156.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.977ca156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d5aabd2bd1c8c26b6c",
+    "url": "/static/js/main.923da7ad.chunk.js"
+  },
+  {
+    "revision": "f3c5c21ece6969d8a3e0",
+    "url": "/static/js/runtime-main.859246b9.js"
+  }
+]);
